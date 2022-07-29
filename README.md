f3210f31693adf12fc5c596288beead4b8bec007
Christian Svensson <blue@cmd.nu>
2022-07-29 20:02:23 +0200

    Set up cluster

---
+++ README.md
@@ -0,0 +1,4 @@
+# elixir-cluster
+
+Start it by running `docker compose up`. It will set up a 4 node cluster and
+connect the nodes to eachother 5 seconds after start up.
--- cluster_agent/lib/cluster_agent.ex
+++ cluster_agent/lib/cluster_agent.ex
@@ -8,16 +8,17 @@ defmodule ClusterAgent do
     listener()
   end
 
-  def sender(primary_agent) do
+  def sender do
     Process.sleep(1000)
+    primary_agent = :global.whereis_name(:primary_agent)
     send(primary_agent, {:hello, node()})
-    sender(primary_agent)
+    sender()
   end
 
   def start(_type, _args) do
     listener = spawn(&listener/0)
     IO.inspect(:global.register_name(:primary_agent, listener))
-    spawn(fn -> sender(:global.whereis_name(:primary_agent)) end)
+    spawn(&sender/0)
     Supervisor.start_link([], strategy: :one_for_one)
   end
 end
